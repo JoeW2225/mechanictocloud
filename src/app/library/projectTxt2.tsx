10beb680e9cddd910585842bc646f7f9a953fd5c
JOE WINTER <joewint3r@gmail.com>
2024-11-26 19:57:42 +0000

    accordion, grow switch imgs, styling added to projpg

---
+++ src/app/library/projectTxt2.tsx
@@ -0,0 +1,9 @@
+export const Tbc = () => {
+    return (
+        <div className="flex flex-col gap-4 sm:w-fit max-w-[65ch]">
+            <div className="w-fit bg-none flex justify-center">
+                <p>Project details pending...⏳</p>
+            </div>
+        </div>
+    )
+}
