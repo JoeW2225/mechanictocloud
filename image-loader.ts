96b58c4c02d12618d9f9fafafbb916adb82fe838
Joe <joewint3r@gmail.com>
2024-12-09 20:56:39 +0000

    amended image-loader.ts

--- image-loader.ts
+++ image-loader.ts
@@ -1,3 +1,5 @@
+'use client'
+
 export default function customImageLoader({ src, width, quality }: { src: string; width: number; quality?: number }) {
     return `https://dcwua42m3tnbq.cloudfront.net/out${src}?w=${width}&q=${quality || 75}`;
 
