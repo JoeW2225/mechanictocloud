776a79ef2a369355b80aa702ef1bb3c5cdcd9b31
Joe <joewint3r@gmail.com>
2024-12-10 11:07:05 +0000

    amended image-loader to use .env

--- image-loader.ts
+++ image-loader.ts
@@ -1,6 +1,10 @@
 'use client'
 
+// process.env. is another way of saying import this
+const distributionId = process.env.NEXT_PUBLIC_CF_DISTRIBUTION_ID;
+
+
 export default function customImageLoader({ src, width, quality }: { src: string; width: number; quality?: number }) {
-    return `https://dcwua42m3tnbq.cloudfront.net/out${src}?w=${width}&q=${quality || 75}`;
+    return `https://${distributionId}.cloudfront.net/out${src}?w=${width}&q=${quality || 75}`;
 
 }
