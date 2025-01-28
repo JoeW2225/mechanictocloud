8532a44b8d2331346da399a443958b1df5a98dec
Joe <joewint3r@gmail.com>
2025-01-28 18:21:49 +0000

    infoTxt change back

--- src/app/library/introTxt.tsx
+++ src/app/library/introTxt.tsx
@@ -3,7 +3,7 @@ return (
     <>
     <div className="flex flex-col gap-4 sm:w-fit max-w-[100ch]">
         <div className="w-fit bg-none flex justify-center">
-        On this site, you&#39;ll learn more about my journey; where I&#39;ve been, where I am, and where I&#39;m headed. Tesing123 
+        On this site, you&#39;ll learn more about my journey; where I&#39;ve been, where I am, and where I&#39;m headed. 
         <br></br>You&#39;ll find a showcase of my latest projects, blending cloud computing and web development, 
         as well as a personal journal where I share in-depth insights into my transition to the tech industry.
         </div>
